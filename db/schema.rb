d06ab958f7a63f396aac977e5051cb23fecea33b
Ademola <ademolaogunmokun@yahoo.com>
2022-02-10 08:10:48 +0100

    add user_id and post_id to comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_09_134010) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_10_070324) do
   create_table "comments", force: :cascade do |t|
-    t.string "author"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
