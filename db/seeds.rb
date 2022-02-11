8376fa08d68636506c954427ff2f521248ab6df1
Olayinka Bidemi Ayeni <layinka4dat@gmail.com>
2022-02-11 10:44:55 +0100

    linters error resolve

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,5 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-
-User.create(username: "Ademola", email: "ademola@me.com", password: "12345")
-Post.create(title: "Active record titile", body: "This is the body of the post")
+User.create(username: 'Ademola', email: 'ademola@me.com', password: '12345')
+Post.create(title: 'Active record titile', body: 'This is the body of the post')
