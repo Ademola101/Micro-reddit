1c9f07b57e59a79401e966b8bf663242cb31d60a
Olayinka Bidemi Ayeni <layinka4dat@gmail.com>
2022-02-11 07:15:57 +0100

    linters error resolve

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   validates :username, length: { in: 3..10 }, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: {in: 5..10}
+  validates :password, presence: true, length: { in: 5..10 }
   has_many :posts
   has_many :comments
 end
